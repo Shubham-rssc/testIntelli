476750e1727d945a25c8fba6435591647ed91d47
Shubham-rssc <cu.16bcs1180@gmail.com>
2023-08-04 16:31:52 +0530

    motogp bike and ferrari instances have been created

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,15 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
-        }
+        Bike motogp = new Bike();
+        motogp.changeSpeed(100);
+        motogp.gearChange(5);
+        motogp.printStates();
+
+        Car ferrari = new Car(6, 200);
+        ferrari.changeSpeed(200);
+        ferrari.gearChange(6);
+        ferrari.printStates();
     }
 }
 
